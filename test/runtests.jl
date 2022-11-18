f62b2d025a4ac2ba6ab830cc24033f1bcaefb1df
Ben Ide <ben@bencide.com>
2022-11-17 22:59:54 -0500

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,33 @@ using GrayCodeIterator
 using Test
 
 @testset "GrayCodeIterator.jl" begin
-    # Write your tests here.
+
+    # check if we get the correct number of items in the iterator
+    @test count(true for v in GrayCode(23,13)) == length(GrayCode(23,13))
+
+    # make sure Base.in is defined properly
+    @test [0,0,1,1,0,1] ∈ GrayCode(6,3)
+    @test [0,0,1,1,0,1] ∈ GrayCode(6,3,[0,0])
+    @test [0,0,1,1,0,1] ∉ GrayCode(6,3,[1,0])
+    @test [0,0,1,1,0,1] ∉ GrayCode(6,2)
+    @test [0,0,1,1,0,1] ∉ GrayCode(6,2,[0,0,1])
+    @test [0,0,1,1,0,1] ∉ GrayCode(5,3)
+
+    # check a particular case, but order doesn't matter
+    V_check = collect(GrayCode(5,2))
+    V_correct = [[1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]
+                 [1, 0, 0, 0, 1]]
+    @test length(V_check) == length(intersect(V_check, V_correct)) == 10
+
+    # make sure eltype is correct
+    @test eltype(GrayCode(5,2)) == typeof(first(GrayCode(5,2)))
 end
+
